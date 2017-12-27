2e1be806a3a4cb902aa982dfeeeafdfd59c449e1
Tim <user@MacBook-Air-Timur.local>
2017-12-27 16:09:24 +0300

    добавлен пустой проект с подключенными CoreData и тестами

---
+++ DTMWeatherApplication/DTMWeatherApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DTMWeatherApplication
+//
+//  Created by user on 24.12.2017.
+//  Copyright © 2017 Davletshin Timur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
